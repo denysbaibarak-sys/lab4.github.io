da53b81a4c177a3ee1c1ecefd576ac6c7886a9d1
Denys Baibarak <denysbaibarak@gmail.com>
2025-11-30 19:16:16 +0200

    index.php

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
 $filename = "counter.txt";
 
-// Якщо файл не існує — створюємо і записуємо 0
+
 if (!file_exists($filename)) {
     $file = fopen($filename, "w");
     fwrite($file, "0");
     fclose($file);
 }
 
-// Відкриваємо файл для читання
+
 $file = fopen($filename, "r");
 
-// Перевіряємо розмір файлу
+
 $filesize = filesize($filename);
 
-// Якщо файл порожній, кількість = 0, інакше читаємо
+
 if ($filesize > 0) {
     $count = fread($file, $filesize);
 } else {
@@ -23,10 +23,10 @@ if ($filesize > 0) {
 
 fclose($file);
 
-// Збільшуємо на 1
+
 $count = (int)$count + 1;
 
-// Відкриваємо файл для запису і оновлюємо значення
+
 $file = fopen($filename, "w");
 fwrite($file, $count);
 fclose($file);
